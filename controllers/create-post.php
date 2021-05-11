88a87d42b1af1ad8683a56d9867b20fbd2727ecf
Omid.N <nejadabbasio@gmail.com>
2021-05-11 21:36:05 +0430

    get the post from $_POST and insert into table

--- controllers/create-post.php
+++ controllers/create-post.php
@@ -6,4 +6,23 @@ class CreatePostController
     {
         return require 'views/create-post.view.php';
     }
+
+    public function savePost() {
+        
+        $title = $_POST['title'];
+        $absract = $_POST['abstract'];
+        $content = $_POST['content'];
+
+        $lastModified = new DateTime();
+        $lastModified = $lastModified->format('Y-m-d');
+        $authorId = 0; // TODO authentication system
+
+
+        $post = new Post($content, $lastModified, $title, $absract, $authorId);
+        
+        PostsTable::getInstance()->insertPost($post);
+
+        header("Location: /");
+    }
+
 }
