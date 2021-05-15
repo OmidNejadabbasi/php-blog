2a9a66cd2fe619ce12f264cc50f3111168a71b40
Omid.N <nejadabbasio@gmail.com>
2021-05-15 23:30:43 +0430

    fixed bugs in uploading files

--- controllers/create-post.php
+++ controllers/create-post.php
@@ -19,10 +19,10 @@ class CreatePostController
         $authorId = 0; // TODO authentication system
 
         $ext = pathinfo($_FILES['poster-image']['name'], PATHINFO_EXTENSION);
-        $posterFileName = 'img-' . str_replace('\s+', '-', $title) . $ext;
-        $target = 'uploads/';
-        move_uploaded_file($$_FILES['poster-image']['tmp_name'], $target . $posterFileName);
-
+        $posterFileName = 'img-' . str_replace('\s+', '-', $title) . "." .  $ext;
+        $target = './uploads/';
+        move_uploaded_file($_FILES['poster-image']['tmp_name'], $target . $posterFileName);
+        
         $post = new Post($content, $lastModified, $title, $absract, $authorId, $posterFileName, $posterFileName);
 
         PostsTable::getInstance()->insertPost($post);
